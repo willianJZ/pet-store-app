fb44c8a6c5bc90d309ed74c294db6fc9428ec01b
wiliZambrano <jesuscod2010@gmail.com>
2024-08-15 21:59:06 -0500

    creacion de nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
