fa627bfaa8ba9c25c2bdf91fb28171806175dbe4
wiliZambrano <jesuscod2010@gmail.com>
2024-09-02 18:04:48 -0500

    Uso de ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,90 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pet-store-app';
+}*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-prueba';
+	// Array de Objetos de productos
+  products = [
+    {
+      name: 'Bike_1',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, 
+    {
+      name: 'Bike_2',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+      name: 'Bike_3',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, 
+    {
+      name: 'Bike_4',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+      name: 'Bike_5',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, 
+    {
+      name: 'Bike',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+      name: 'Bike_6',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, 
+    {
+      name: 'Bike_7',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+      name: 'Bike_8',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }, 
+    {
+      name: 'Bike_9',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+ ]
 }
+
+
